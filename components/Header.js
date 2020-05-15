2bd8243f6b57e46b159260e147c5ac1a73e0891d
corvuscornix <android.tanskanen@gmail.com>
2020-05-15 20:18:41 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,33 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const linkStyle = {
   marginRight: 15,
 };
 
+const pages = {
+  '/': 'Home',
+  '/asteroids': 'Asteroids',
+  '/solarflares': 'Solar flares',
+};
+
 export default function Header() {
+  const router = useRouter();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push(href);
+  };
+
   return (
     <div>
       <div className="header">Next level web service</div>
       <div className="navigation">
-        <Link href="/">
-          <a style={linkStyle}>Home</a>
-        </Link>
-        <Link href="/asteroids">
-          <a style={linkStyle}>Asteroids</a>
-        </Link>
-        <Link href="/solarflares">
-          <a style={linkStyle}>Solar flares</a>
-        </Link>
+        {Object.entries(pages).map((page) => (
+          <Link href={page[0]}>
+            <a style={Object.assign(linkStyle, router.pathname === page[0] && { background: 'yellow' })} onClick={handleClick}>{page[1]}</a>
+          </Link>
+        ))}
       </div>
 
       <style jsx>
@@ -28,9 +38,30 @@ export default function Header() {
           }
 
           .navigation {
-            border-top: 1px solid #DDD;
-            border-bottom: 1px solid #DDD;
             padding: 8px 0;
+            position: relative;
+            display: flex;
+            justify-content: space-between;
+            height: auto;
+          }
+
+          a {
+            background: gray;
+            padding: 16px;
+            margin: 0 8px;
+            color: white;
+            flex-grow: 1;
+            text-align: center;
+            text-transform: uppercase;
+            text-decoration: none;
+          }
+
+          a:hover {
+            background: red;
+          }
+
+          a.active {
+            background: green;
           }
         `}
       </style>
